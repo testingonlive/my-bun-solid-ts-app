dee0fb928d5d112c6ca97d9c0dc40159408c3f9d
James Wilson <james.wilson537@gmail.com>
2024-04-25 22:56:49 +0100

    nothing change

--- src/algoliaFetch.ts
+++ src/algoliaFetch.ts
@@ -3,12 +3,12 @@ import algoliasearch from 'algoliasearch/lite';
 const client = algoliasearch('latency', '6be0576ff61c053d5f9a3225e2a90f76');
 const index = client.initIndex('instant_search');
 
-type AlgoliaResponse1 = {
+type AlgoliaResponse = {
     name: string;
 }
 
 const algoliaFetch = (query: string) => {
-    return index.search<AlgoliaResponse1>(query)
+    return index.search<AlgoliaResponse>(query)
 }
 
 export default algoliaFetch;
